7a22e12aae6de03159b8132ad38401e2a27fb30b
DabaniCode <dabani.rm@gmail.com>
2021-04-25 11:48:21 +0200

    Facebook Strategy

---
+++ passport/facebook.js
@@ -0,0 +1,22 @@
+const passport = require('passport');
+const facebookStrategy = require('passport-facebook').Strategy;
+const User = require('../models/user');
+const keys = require('../config/keys');
+
+passport.serializeUser((user,done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id,(err, id) => {
+    done(err, user);
+  });
+});
+
+passport.use(new facebookStrategy({
+  clientID: keys.FacebookAppId,
+  clientSecret: keys.FacebookAppSecret,
+  callbackURL: 'http://localhost:3000/auth/facebook/callback'
+}, (accessToken, refreshToken, profile, done) => {
+  console.log(profile);
+}));
