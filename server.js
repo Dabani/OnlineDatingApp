975dad184b179b33f657870f338f6e80ce3af2f7
DabaniCode <dabani.rm@gmail.com>
2021-04-28 10:05:44 +0200

    Google Auth

--- server.js
+++ server.js
@@ -74,6 +74,7 @@ app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redi
 app.use('/js', express.static(__dirname + '/node_modules/@popperjs/core/dist/umd')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap-icons/font/')); // redirect CSS bootstrap-icons
+app.use('/css', express.static(__dirname + '/assets/css')); // redirect CSS app
 
 app.get('/', ensureGuest, (req,res) => {
   res.render('home',{
