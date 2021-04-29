eecdadc0f9d072b877a114475fc5a9dec20c8209
DabaniCode <dabani.rm@gmail.com>
2021-04-29 11:31:53 +0200

    MongoDB link updated

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-  MongoDB: 'mongodb+srv://onlinedatingapp:onlinedatingapp1@onlinedatingapp.m6dxp.mongodb.net/OnlineDatingApp?retryWrites=true&w=majority',
+  MongoDB: process.env.MONGO_DB,
   FacebookAppId: '304215291140681',
   FacebookAppSecret: '9aa8ebfb2294335a2c3098319749931f',
   GoogleClientId: '935280856065-0it9bruneb5q5viakgt8pq8o42m6j5uq.apps.googleusercontent.com',
