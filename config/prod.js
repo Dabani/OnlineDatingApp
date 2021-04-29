adebe8927774c48a09a3f255b15e77ea4369e9ee
DabaniCode <dabani.rm@gmail.com>
2021-04-29 12:07:19 +0200

    Facebook ID and Secret for production

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 module.exports = {
   MongoDB: process.env.MONGO_DB,
-  FacebookAppId: '304215291140681',
-  FacebookAppSecret: '9aa8ebfb2294335a2c3098319749931f',
+  FacebookAppId: process.env.FB_APP_ID,
+  FacebookAppSecret: process.env.FB_APP_SECRET,
   GoogleClientId: '935280856065-0it9bruneb5q5viakgt8pq8o42m6j5uq.apps.googleusercontent.com',
   GoogleClientSecret: '-FFWodgmDvvVMcPcJ-NA9Wft'
 }
