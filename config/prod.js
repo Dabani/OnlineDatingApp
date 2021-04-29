7f00ac7338f0a66d3b78ecb447a85c748e6f1ce6
DabaniCode <dabani.rm@gmail.com>
2021-04-29 13:55:18 +0200

    Google Client ID for production

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,6 @@ module.exports = {
   MongoDB: process.env.MONGO_DB,
   FacebookAppId: process.env.FB_APP_ID,
   FacebookAppSecret: process.env.FB_APP_SECRET,
-  GoogleClientId: '935280856065-0it9bruneb5q5viakgt8pq8o42m6j5uq.apps.googleusercontent.com',
-  GoogleClientSecret: '-FFWodgmDvvVMcPcJ-NA9Wft'
+  GoogleClientId: process.env.GOOGLE_CLIENT_ID,
+  GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 }
