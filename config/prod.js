7986eadef54c8d045e9511f418df36606e3dbc55
DabaniCode <dabani.rm@gmail.com>
2021-04-29 09:28:00 +0200

    Push changes to heroku

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+  MongoDB: 'mongodb+srv://onlinedatingapp:onlinedatingapp1@onlinedatingapp.m6dxp.mongodb.net/OnlineDatingApp?retryWrites=true&w=majority',
+  FacebookAppId: '304215291140681',
+  FacebookAppSecret: '9aa8ebfb2294335a2c3098319749931f',
+  GoogleClientId: '935280856065-0it9bruneb5q5viakgt8pq8o42m6j5uq.apps.googleusercontent.com',
+  GoogleClientSecret: '-FFWodgmDvvVMcPcJ-NA9Wft'
+}
