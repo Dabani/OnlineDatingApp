4826f08267241ad376eb3a58f27bdecb0c7a520b
DabaniCode <dabani.rm@gmail.com>
2021-04-29 14:03:46 +0200

    Google call back redirect

--- passport/google.js
+++ passport/google.js
@@ -16,7 +16,7 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
   clientID: keys.GoogleClientId,
   clientSecret: keys.GoogleClientSecret,
-  callbackURL: 'http://localhost:3000/auth/google/callback'
+  callbackURL: 'https://warm-wildwood-26257.herokuapp.com/auth/google/callback'
 },(accessToken, refreshToken, profile, done) => {
   console.log(profile);
 
