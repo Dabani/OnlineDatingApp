56134b81128617a91e36575e4cf4b46c4ee0a005
DabaniCode <dabani.rm@gmail.com>
2021-05-13 23:04:12 +0200

    Delete Chat

--- server.js
+++ server.js
@@ -549,6 +549,14 @@ app.get('/chats', requireLogin, (req, res) => {
   })
 });
 
+// Delete chat
+app.get('/deleteChat/:id', requireLogin, (req, res) => {
+  Chat.deleteOne({_id:req.params.id})
+  .then(() => {
+    res.redirect('/chats');
+  });
+});
+
 // Get route to send smile
 app.get('/sendSmile/:id', requireLogin, (req, res) => {
   const newSmile = {
