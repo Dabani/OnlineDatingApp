84e0d89883c663e6a5dd90bdbd8f522b546bf987
DabaniCode <dabani.rm@gmail.com>
2021-05-15 23:10:48 +0200

    Deploy changes to Heroku

--- config/prod.js
+++ config/prod.js
@@ -7,5 +7,7 @@ module.exports = {
   GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   GoogleCallBackUrl: process.env.GOOGLE_CALLBACK_URL,
   AWSAccessKeyID: process.env.AWS_ACCESS_KEY_ID,
-  AWSAccessKeySecret: process.env.AWS_SECRET_ACCESS_KEY
+  AWSAccessKeySecret: process.env.AWS_SECRET_ACCESS_KEY,
+  StripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  StripeSecretKey: process.env.STRIPE_SECRET_KEY
 }
