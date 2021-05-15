26c1c0bf7f19f0c873db4e2625cee972a66a77b1
DabaniCode <dabani.rm@gmail.com>
2021-05-15 13:35:41 +0200

    Payment Form

--- helpers/wallet.js
+++ helpers/wallet.js
@@ -1,8 +1,11 @@
+const keys = require('../config/keys');
+
 module.exports = {
   walletChecker: function(req, res, next) {
     if (req.user.wallet <= 0) {
       res.render('payment/payment', {
-        title: 'Payment'
+        title: 'Kwishyura',
+        StripePublishableKey: keys.StripePublishableKey
       });
     } else {
       return next();
